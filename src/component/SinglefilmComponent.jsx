4c191717898b46b74b2da16a24e07b06d30555c0
Liborio Daniele Frasca <frascaliboriodaniele@gmail.com>
2024-02-02 17:00:41 +0100

    mod

--- src/component/SinglefilmComponent.jsx
+++ src/component/SinglefilmComponent.jsx
@@ -2,11 +2,14 @@ import React, { Component } from "react";
 import Image from "react-bootstrap/Image";
 import Col from "react-bootstrap/Col";
 import SpinnerComponent from "./SpinnerComponent";
+import Alert from "react-bootstrap/Alert";
+import { toBeRequired } from "@testing-library/jest-dom/matchers";
 
 class SinglefilmComponent extends Component {
   state = {
     movies: [],
     isLoaded: false,
+    error: false,
   };
 
   fetchMovies = () => {
@@ -24,6 +27,7 @@ class SinglefilmComponent extends Component {
         this.setState({ movies: data.Search, isLoaded: false });
       })
       .catch((error) => console.error("Error fetching movies:", error));
+    this.setState({ isLoaded: false, error: true });
   };
 
   componentDidMount() {
@@ -32,30 +36,29 @@ class SinglefilmComponent extends Component {
 
   render() {
     return this.state.isLoaded ? (
-      <div className="d-flex justify-content-center">
+      <div>
         <SpinnerComponent />
       </div>
     ) : (
-      this.state.movies.splice(0, 6).map((film) => {
-        return (
-          <Col
-            xs={12}
-            sm={4}
-            md={3}
-            lg={2}
-            xl={2}
-            className="p-3"
-            key={film.imdbID}
-          >
-            <Image
-              src={film.Poster}
-              width="100%"
-              height="100%"
-              className="scale"
-            />
+      <>
+        {this.state.error && (
+          <Col>
+            <Alert variant="danger">Si è verificato un problema</Alert>
           </Col>
-        );
-      })
+        )}
+        {this.state.movies.slice(0, 6).map((film) => (
+          <Col key={film.imdbID} sm={2} md={4} lg={3}>
+            {film.Poster && (
+              <Image
+                src={film.Poster}
+                width="100%"
+                height="100%"
+                className="scale"
+              />
+            )}
+          </Col>
+        ))}
+      </>
     );
   }
 }
